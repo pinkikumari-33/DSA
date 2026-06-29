c7948205204512204368c8c919d50fc4d213aa1c
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-06-29 22:44:09 +0530

    Time: 8 ms (29.35%) | Memory: 26.5 MB (14.89%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size();i++){
+            mp[nums[i]]++;
+        }
+
+        vector<int> output;
+
+        for(auto it : mp){
+            if(it.second > (nums.size()/3)){
+                output.push_back(it.first);
+            }
+        }
+
+        return output;
+    }
+};
