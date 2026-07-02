56b8b8e6afa34918e68686455c93ed93dc5736f0
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-02 23:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (50.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
+        int row = matrix.size();
         int col = matrix[0].size();
-        
-        vector<int>rown;
-        vector<int>colm;
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < col; j++){
+        int col0 = 1;
+        bool row0 = 1;
+
+        for(int i = 0; i < row; i++){
+            if(matrix[i][0] == 0) col0 = 0;
+        }
+
+        for(int i = 0; i < col; i++){
+            if(matrix[0][i] == 0) row0 = 0;
+        }
+
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
                 if(matrix[i][j] == 0){
-                    rown.push_back(i);
-                    colm.push_back(j);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        int j = 0;
-        for(int i :  rown){
-            for(j = 0; j < col;j++){
-                matrix[i][j] = 0;
-            }  
-        } 
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
-        int k = 0;
-        for(int i: colm){
-            for(k = 0; k < rows;k++){
-                matrix[k][i] = 0;
-            }  
-        } 
-        
+        if(col0 == 0){
+            for(int i = 0; i < row; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(row0 == 0){
+            for(int i = 0; i < col; i++){
+                matrix[0][i] = 0;
+            }
+        }
     }
 };
