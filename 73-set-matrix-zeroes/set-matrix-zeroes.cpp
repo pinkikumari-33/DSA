89d022b892e380bdd0d2b359c960534a1dbc503d
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-02 22:19:48 +0530

    Time: 191 ms (5.31%) | Memory: 22 MB (5.32%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int col = matrix[0].size();
+        
+        vector<int>rown;
+        vector<int>colm;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < col; j++){
+                if(matrix[i][j] == 0){
+                    cout<<i<<" "<<j<<endl;
+                    rown.push_back(i);
+                    colm.push_back(j);
+                }
+            }
+        }
+
+        int j = 0;
+        for(int i :  rown){
+            for(j = 0; j < col;j++){
+                matrix[i][j] = 0;
+            }  
+        } 
+
+        int k = 0;
+        for(int i: colm){
+            for(k = 0; k < rows;k++){
+                matrix[k][i] = 0;
+            }  
+        } 
+        
+    }
+};
