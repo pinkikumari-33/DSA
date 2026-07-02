fe829464dc068a538455f27b218398058b65e922
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-02 22:20:06 +0530

    Time: 23 ms (16.49%) | Memory: 21.8 MB (8.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -10,7 +10,6 @@ public:
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < col; j++){
                 if(matrix[i][j] == 0){
-                    cout<<i<<" "<<j<<endl;
                     rown.push_back(i);
                     colm.push_back(j);
                 }
