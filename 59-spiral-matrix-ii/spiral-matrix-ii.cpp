72c70adf92bbca7e53c59bfb550081a42e07dc3f
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-03 12:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.78%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n,vector<int>(n));
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int srow = 0, erow = row -1;
+        int scol = 0, ecol = col -1;
+
+        int val = 1;
+
+        while(srow <= erow && scol <= ecol){
+            // Top Boundary
+            for(int i = scol; i <= ecol; i++) matrix[srow][i] = val++;
+
+            // Right Boundary
+            for(int i = srow+1; i <= erow; i++) matrix[i][ecol] = val++;
+
+            // Bottom Boundary
+            for(int i = ecol-1; i >= scol; i--){
+                if(srow == erow) break;
+                matrix[erow][i] = val++;
+            }
+
+            // Left Boundary
+            for(int i = erow-1; i>= srow+1; i--){
+                if(ecol == scol) break;
+                matrix[i][scol] = val++;
+            }
+
+            srow++;scol++;
+            erow--;ecol--;
+        }
+
+        return matrix;
+        
+    }
+};
