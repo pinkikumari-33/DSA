4888f6f62592b1e7be0b63323685cebb29b51920
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-03 21:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.30%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows,vector<int>(1));
+
+        if(numRows == 0) return {};
+
+        ans[0][0] = 1;
+        
+        if(numRows == 1) return ans;
+
+        for(int i = 1; i < numRows; i++){
+            ans[i].resize(i+1);
+
+            for(int j = 0; j <= i; j++){
+
+                if(j == 0){
+                   ans[i][j] = 1;
+                } 
+                else if(i == j){
+                   ans[i][j] = 1;
+                } 
+                else{
+                   ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
