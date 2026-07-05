2de49926770a216a013e271e75605a3e27f71708
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-05 20:45:46 +0530

    Time: 4 ms (65.80%) | Memory: 23.9 MB (59.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,36 +2,19 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-
-        if(n == 1) return intervals;
-
         sort(intervals.begin(),intervals.end());
 
-        for(vector<int> i : intervals){
-            cout<<i[0]<<" "<<i[1]<<endl;
-        }
-
-        int i = 0;
-        int j = i + 1;
-
         vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
 
-        while(j < n){
-            if(intervals[i][1] >= intervals[j][0]){
-                while(j < n && intervals[i][1] >= intervals[j][0]){
-                    intervals[i][1] = max(intervals[i][1],intervals[j][1]);
-                    j++;
-                }
-                ans.push_back({intervals[i][0],intervals[i][1]});
+        for(int i = 1;i < n; i++){
+            if(ans.back()[1] >= intervals[i][0]){
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
             }else{
-                ans.push_back({intervals[i][0],intervals[i][1]});
+                ans.push_back(intervals[i]);
             }
-            i = j;
-            j = i + 1;   
         }
 
-        if(i < n) ans.push_back(intervals[i]);
-
-        return ans;        
+        return ans;
     }
 };
