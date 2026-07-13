e8aa151c2a481cbafb9baee8fc76db5afc98d297
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-13 11:17:53 +0530

    Time: 14 ms (11.91%) | Memory: 13.2 MB (50.58%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -14,37 +14,15 @@ public:
         int n = s.length();
         int value = 0;
 
-        for(int i = 0;i < n; i++){
-            if(i > 0 && s[i] == 'V' && s[i-1] == 'I'){
-                value += 3;
+        for(int i = 0; i < n -1; i++){
+            if(roman[s[i]] < roman[s[i+1]]){
+                value -= roman[s[i]];
             }
-
-            else if(i > 0 && s[i] == 'X' && s[i-1] == 'I'){
-                value += 8;
-                i++;
-            }
-
-            else if(i > 0 && s[i] == 'C' && s[i-1] == 'X'){
-                value += 80;
-            }
-
-            else if(i > 0 && s[i] == 'L' && s[i-1] == 'X'){
-                value += 30;
-            }
-
-            else if(i > 0 && s[i] == 'D' && s[i-1] == 'C'){
-                value += 300;
-            }
-
-            else if(i > 0 && s[i] == 'M' && s[i-1] == 'C'){
-                value += 800;
-            }
-
             else{
                 value += roman[s[i]];
             }
         }
 
-        return value;
+        return value+roman[s[n-1]];
     }
 };
