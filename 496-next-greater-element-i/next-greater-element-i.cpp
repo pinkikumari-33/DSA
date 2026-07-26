22ca784d3a32b419160571450c4305e4f1d864f2
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-26 21:54:07 +0530

    Time: 4 ms (22.98%) | Memory: 12.8 MB (62.99%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,22 +4,29 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        vector<int> ans(n1,-1);
+        unordered_map<int,int> mp;
+        stack<int> st;
 
-        for(int i=0; i < n1; i++){
-            for(int j=0; j < n2; j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j+1; k < n2; k++){
-                        if(nums2[k] > nums2[j]){
-                            ans[i] = nums2[k];
-                            break;
-                        }
-                    }
-                }
+        for(int i = n2-1; i >= 0; i--){
+            while(!st.empty() && st.top() < nums2[i]){
+                st.pop();
             }
+
+            if(st.empty()){
+                mp[nums2[i]] = -1;
+            }else{
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans(n1);
+
+        for(int i = 0; i < n1; i++){
+            ans[i] = mp[nums1[i]];
         }
 
         return ans;
-
     }
 };
