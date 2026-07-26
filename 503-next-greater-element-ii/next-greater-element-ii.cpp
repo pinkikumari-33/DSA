4855179e8a3729c0d4d537b8c9e2090565388938
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-26 22:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (40.04%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        //unordered_map<int,int> mp;
+        stack<int> st;
+
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 2*n-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i % n]){
+                st.pop();
+            }
+
+            if(i < n){
+                if(st.empty()){
+                   ans[i] = -1;
+                }
+                else{
+                    ans[i] = st.top();
+                }
+            }
+            
+            st.push(nums[i % n]);
+        }
+
+        return ans;
+    }
+};
