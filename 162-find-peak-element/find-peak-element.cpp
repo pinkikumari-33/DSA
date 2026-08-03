66b91f8e66b5aafe8fa27c8579b30f4469ee885d
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-08-03 20:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (41.42%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+    int binarySearch(vector<int> &nums,int left,int right){
+        int mid = left + (right-left)/2;
+
+        if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
+            return mid;
+        }
+
+        else if(nums[mid-1] < nums[mid]){
+            return binarySearch(nums,mid+1,right);
+        }
+        else{
+            return binarySearch(nums,left,mid-1);
+        }
+    }
+
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
+
         if(n == 1) return 0;
 
         if(nums[0] > nums[1]) return 0;
-
         if(nums[n-1] > nums[n-2]) return n-1;
 
-        for(int i = 1 ; i < n-1 ; i++){
-            if(nums[i-1] < nums[i] && nums[i] > nums[i+1]){
-                return i;
-            }
-        }
-        
-        return -1;
+        int ans = binarySearch(nums,1,n-2);
+
+        return ans;
     }
 };
