1fcb2ba4a60b8e5b3ece987e4a383994b043f440
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-08-06 22:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (32.27%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -12,14 +12,18 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == nullptr) return nullptr;
-
-        if(root->val == val) return root;
-
-        if(root->val > val){
-            return searchBST(root->left,val);
-        }else{
-            return searchBST(root->right,val);
+        while(root != nullptr){
+            if(root->val == val){
+                return root;
+            }
+            else if(root -> val > val){
+                root = root->left;
+            }
+            else{
+                root = root->right;
+            }
         }
+
+        return nullptr;
     }
 };
