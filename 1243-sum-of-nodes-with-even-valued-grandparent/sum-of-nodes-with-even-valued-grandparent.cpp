70ff4a64226c8d90e8c1cecc2b80b96332ab8e28
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-09-13 11:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (34.34%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,60 +11,23 @@
  */
 class Solution {
 public:
-    bool grandParent(TreeNode* root) {
-        if(root == nullptr) return false;
 
-        if(root->left){
-            if(root->left->left != nullptr || root->left->right != nullptr) {
-                return true;
-            }
-        }
-
-        if(root->right) {
-            if(root->right->left != nullptr || root->right->right != nullptr) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    void helperFunction(TreeNode* root,int &sum) {
+    void helperFunction(TreeNode* root,TreeNode* grandParent,TreeNode* parent,int &sum) {
         if(root == nullptr) {
             return;
         }
 
-        if(grandParent(root)) {
-            if(root->val % 2 == 0) {
-                    if(root -> left){
-                        if(root->left->left) {
-                        sum += root->left->left->val;
-                    }
-
-                    if(root->left->right) {
-                        sum += root->left->right->val;
-                    }
-                }
-
-                if(root->right) {
-                    if(root->right->left) {
-                        sum += root->right->left->val;
-                    }
-
-                    if(root->right->right) {
-                        sum += root->right->right->val;
-                    }
-                }
-            }
+        if(grandParent != nullptr && grandParent->val % 2 == 0) {
+            sum += root->val;
         }
 
-        helperFunction(root->left,sum);
-        helperFunction(root->right,sum);
+        helperFunction(root->left,parent,root,sum);
+        helperFunction(root->right,parent,root,sum);
     }
 
     int sumEvenGrandparent(TreeNode* root) {
         int sum = 0;
-        helperFunction(root,sum);
+        helperFunction(root,nullptr,nullptr,sum);
 
         return sum;
     }
