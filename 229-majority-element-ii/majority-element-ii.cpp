4852e7df122eb5b0656b554b20118d16769cc501
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-09-15 05:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (52.64%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,59 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        vector<int> maj;
+        vector<int> ans;
+
+        int cand1 = 0;
+        int cand2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
 
         int n = nums.size();
 
         for(int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
+            if(count1 == 0 && nums[i] != cand2) {
+                cand1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0 && nums[i] != cand1) {
+                cand2 = nums[i];
+                count2 = 1;
+            }
 
-        for(auto it  : mp) {
-            if(it.second > n/3) {
-                maj.push_back(it.first);
+            else if(nums[i] == cand1) {
+                count1++;
+            }
+
+            else if(nums[i] == cand2) {
+                count2++;
+            }
+
+            else {
+                count1--;
+                count2--;
             }
         }
 
-        return maj;
+        int freq1 = 0;
+        int freq2 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == cand1) freq1++;
+            if(nums[i] == cand2) freq2++;
+        }
+
+        if(cand1 == cand2 && freq1 > n/3) {
+            ans.push_back(cand1);
+            return ans;
+        }
+
+        if(freq1 > n/3) ans.push_back(cand1);
+        if(freq2 > n/3) ans.push_back(cand2);
+
+        return ans;
+
+
     }
+
 };
