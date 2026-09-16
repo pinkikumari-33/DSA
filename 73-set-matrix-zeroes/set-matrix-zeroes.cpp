3097ba3a34c705aa2edb1a537c9ca3664aa7f60f
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-09-17 05:13:20 +0530

    Time: 23 ms (9.65%) | Memory: 22.1 MB (6.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,44 +3,30 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
+        cout<<row<< " "<<col;
 
-        int col0 = 1;
-        bool row0 = 1;
+        vector<int> rowVal;
+        vector<int> colVal;
 
-        for(int i = 0; i < row; i++){
-            if(matrix[i][0] == 0) col0 = 0;
-        }
-
-        for(int i = 0; i < col; i++){
-            if(matrix[0][i] == 0) row0 = 0;
-        }
-
-        for(int i = 1; i < row; i++){
-            for(int j = 1; j < col; j++){
-                if(matrix[i][j] == 0){
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(matrix[i][j] == 0) {
+                    rowVal.push_back(i);
+                    colVal.push_back(j);
                 }
             }
         }
 
-        for(int i = 1; i < row; i++){
-            for(int j = 1; j < col; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
+
+        for(int x : rowVal) {
+            for(int j = 0; j < col; j++) {
+                matrix[x][j] = 0;
             }
         }
 
-        if(col0 == 0){
-            for(int i = 0; i < row; i++){
-                matrix[i][0] = 0;
-            }
-        }
-
-        if(row0 == 0){
-            for(int i = 0; i < col; i++){
-                matrix[0][i] = 0;
+        for(int x : colVal) {
+            for(int j = 0; j < row; j++) {
+                matrix[j][x] = 0;
             }
         }
     }
